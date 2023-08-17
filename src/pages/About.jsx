bcddbbb4ad49b7b15406c4d9ee30f1136f22d344
Johnny Hsu <yuwei.hsu@dowjones.com>
2023-08-16 22:21:08 -0500

    add margin-top on image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ import WorkInProcessImg from '/images/work_in_process.jpg';
 
 const About = () => {
   return (
-    <div className='d-flex align-items-center justify-content-center h-100'>
+    <div className='d-flex align-items-center justify-content-center h-100 mt-5'>
       <img src={WorkInProcessImg} alt='work in process' />
     </div>
   )
